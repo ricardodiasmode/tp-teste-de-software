5da5e9274fdc1cff164a7fb41dab118d691970b2
Ricardo Avelar <50161316+ricardodiasmode@users.noreply.github.com>
2023-10-20 19:44:40 -0300

    testing background

--- background.py
+++ background.py
@@ -4,17 +4,15 @@ import pygame.display
 
 
 class Background:
-    rounds_without_spawning_log = 2
-    min_rounds_to_spawn_log = 3
-    max_rounds_to_spawn_log = 5
     display_width = 1024
+    debug_width = 275
     display_height = 1024
     basic_square_size = 64
     screen = None
+
     square_image_dict = {}
     square_dict = {}
     log_locations = []
-    last_gap_location = -1
 
     grass0_img = pygame.image.load('img/Grass0.png')
     grass1_img = pygame.image.load('img/Grass1.png')
@@ -23,39 +21,44 @@ class Background:
     log_img = pygame.image.load('img/Log.png')
 
     def __init__(self):
-        self.screen = pygame.display.set_mode((self.display_width + 275, self.display_height))
+        self.screen = pygame.display.set_mode((self.display_width + self.debug_width, self.display_height))
         self.reset_background()
 
     def reset_background(self):
+        self.reset_variables()
+        self.init_background()
+
+    def reset_variables(self):
         self.log_locations = []
         self.square_image_dict = {}
         self.square_dict = {}
-        self.init_background()
 
     def init_background(self):
-        # filling background with grass
-        spawned_at_least_one_log = False
         for current_width in range(0, self.display_width, self.basic_square_size):
             for current_height in range(0, self.display_height, self.basic_square_size):
+                current_location = (current_width, current_height)
+                if self.square_dict.get(current_location) is not None:
+                    continue
+
                 random_number = randrange(5)
                 if random_number == 1:
                     image_to_use = self.log_img
-                    self.log_locations.append((current_width, current_height))
-                    self.square_dict[(current_width, current_height)] = "LOG"
-                    spawned_at_least_one_log = True
+                    if current_location not in self.log_locations:
+                        self.log_locations.append(current_location)
+                    self.square_dict.update({current_location: "LOG"})
                 elif random_number == 2:
                     image_to_use = self.grass1_img
-                    self.square_dict[(current_width, current_height)] = "GRASS"
+                    self.square_dict.update({current_location: "GRASS"})
                 elif random_number == 3:
                     image_to_use = self.grass2_img
-                    self.square_dict[(current_width, current_height)] = "GRASS"
+                    self.square_dict.update({current_location: "GRASS"})
                 else:
                     image_to_use = self.grass3_img
-                    self.square_dict[(current_width, current_height)] = "GRASS"
+                    self.square_dict.update({current_location: "GRASS"})
 
-                self.screen.blit(image_to_use, (current_width, current_height))
-                self.square_image_dict[(current_width, current_height)] = image_to_use
-        if not spawned_at_least_one_log:
+                self.screen.blit(image_to_use, current_location)
+                self.square_image_dict[current_location] = image_to_use
+        if not self.log_locations:
             self.reset_background()
 
     def update_square(self, loc, in_type):
---
+++ test_background.py
@@ -0,0 +1,42 @@
+import background
+
+
+class TestBackground:
+    mocked_background = background.Background()
+
+    def test_screen_width(self):
+        assert self.mocked_background.screen.get_width() == \
+               self.mocked_background.display_width + self.mocked_background.debug_width
+
+    def test_screen_height(self):
+        assert self.mocked_background.screen.get_height() == self.mocked_background.display_height
+
+    def test_variables_reset(self):
+        self.mocked_background.reset_variables()
+        assert self.mocked_background.log_locations == []
+        assert self.mocked_background.square_image_dict == {}
+        assert self.mocked_background.square_dict == {}
+
+    def test_has_logs_on_init(self):
+        self.mocked_background.init_background()
+        assert len(self.mocked_background.log_locations) > 0
+
+    def test_logs_square_dict_and_logs_location_are_equal_in_size(self):
+        self.mocked_background.init_background()
+
+        log_locations = []
+        for key, value in self.mocked_background.square_dict.items():
+            if value == "LOG":
+                log_locations.append(key)
+
+        assert len(self.mocked_background.log_locations) == len(log_locations)
+
+    def test_update_square_to_grass(self):
+        self.mocked_background.reset_variables()
+        self.mocked_background.update_square((0, 0), "GRASS")
+        assert self.mocked_background.square_image_dict[0, 0] == self.mocked_background.grass0_img
+
+    def test_update_square_to_log(self):
+        self.mocked_background.reset_variables()
+        self.mocked_background.update_square((0, 0), "LOG")
+        assert self.mocked_background.square_image_dict[0, 0] == self.mocked_background.log_img
